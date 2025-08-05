1d64a330d82d5f62f77bf51ad59274808fe0c790
TracopkWb <tracopk.wb@gmail.com>
2025-08-05 16:27:57 -0600

    Tables updated

--- controllers/Sql/car.sql
+++ controllers/Sql/car.sql
@@ -9,13 +9,11 @@ CREATE TABLE `car` (
     `carCurrMilage` int(11) DEFAULT NULL,
     `cos_Id` varchar(10) NOT NULL,
     `car_Registration_Date` date DEFAULT NULL,
-    `serv_Id` varchar(10) NOT NULL,
     `car_Photo` varchar(255) DEFAULT NULL,
     `car_Entrance_Date` date NOT NULL,
     `car_Exit_Date` date DEFAULT NULL,
     `car_Status` varchar(10) DEFAULT NULL,
     PRIMARY KEY (`car_Id`),
-    KEY `serv_Id` (`serv_Id`),
     KEY `cos_Id` (`cos_Id`),
     CONSTRAINT `car_ibfk_2` FOREIGN KEY (`cos_Id`) REFERENCES `costumer` (`cos_Id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '	'
@@ -27,22 +25,9 @@ CREATE TABLE `costumer` (
     `cosOtherContacts` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (
         json_valid(`cosOtherContacts`)
     ),
-    `car_Id` varchar(10) NOT NULL,
-    PRIMARY KEY (`cos_Id`),
-    KEY `car_Id` (`car_Id`)
+    PRIMARY KEY (`cos_Id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4
 
-CREATE TABLE `ticket` (
-    `tic_Id` varchar(45) NOT NULL,
-    `serv_Id` varchar(10) NOT NULL,
-    `ticTotal` int(11) NOT NULL,
-    `ticLabor` int(11) NOT NULL,
-    `ticMaterial` int(11) NOT NULL,
-    `ticOther` int(11) NOT NULL,
-    PRIMARY KEY (`tic_Id`),
-    KEY `serv_Id` (`serv_Id`),
-    CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`serv_Id`) REFERENCES `service_record` (`serv_Id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4
 CREATE TABLE `materialused` (
     `mat_Id` varchar(10) NOT NULL,
     `serv_Id` varchar(10) NOT NULL,
@@ -57,6 +42,15 @@ CREATE TABLE `materialused` (
     PRIMARY KEY (`mat_Id`),
     KEY `serv_Id` (`serv_Id`),
     CONSTRAINT `materialused_ibfk_1` FOREIGN KEY (`serv_Id`) REFERENCES `service_record` (`serv_Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4
+
+CREATE TABLE `mechanic` (
+    `mec_Id` varchar(45) NOT NULL,
+    `mecName` varchar(45) NOT NULL,
+    `mecNumber` varchar(45) NOT NULL,
+    `mecPhoto` varchar(225) DEFAULT 'None',
+    `mecBiometrics` int(10) unsigned zerofill DEFAULT NULL,
+    PRIMARY KEY (`mec_Id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4
 
 CREATE TABLE `service_record` (
@@ -77,9 +71,17 @@ CREATE TABLE `service_record` (
     `serRe_milage_at_Service` int(11) DEFAULT NULL,
     `serRe_Finished_Date` date DEFAULT NULL,
     `tic_Id` varchar(45) DEFAULT NULL,
-    `mat_Id` varchar(10) DEFAULT NULL,
     PRIMARY KEY (`serv_Id`),
     KEY `car_Id` (`car_Id`),
-    KEY `mat_Id` (`mat_Id`),
     CONSTRAINT `service_record_ibfk_1` FOREIGN KEY (`car_Id`) REFERENCES `car` (`car_Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4
+
+
+CREATE TABLE `ticket` (
+    `tic_Id` varchar(45) NOT NULL,
+    `serv_Id` varchar(10) NOT NULL,
+    `ticTotal` decimal(10, 2) NOT NULL,
+    `ticDate` date NOT NULL,
+    PRIMARY KEY (`tic_Id`),
+    KEY `serv_Id` (`serv_Id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4
